0e1d9bfd86b965b356cc6dc789b386e28850ce0f
BrunaHosken <bruna16.hosken@gmail.com>
2026-01-04 14:39:53 -0500

    Adicionando endpoint de carros

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 
-# CarModelInline class
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'car_type', 'year', 'dealer_id')
+    list_filter = ('car_type', 'year')
+    search_fields = ('name',)
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'founded_year')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
