2c2914b3539b36e36e9415a648e3e505bca5d139
BrunaHosken <bruna16.hosken@gmail.com>
2026-01-04 17:14:10 -0500

    adicionando review

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -16,18 +16,17 @@ def home():
 def analyze_sentiment(input_txt):
 
     scores = sia.polarity_scores(input_txt)
-    print(scores)
     pos = float(scores['pos'])
     neg = float(scores['neg'])
     neu = float(scores['neu'])
     res = "positive"
-    print("pos neg nue ", pos, neg, neu)
+
     if (neg > pos and neg > neu):
         res = "negative"
     elif (neu > neg and neu > pos):
         res = "neutral"
     res = json.dumps({"sentiment": res})
-    print(res)
+
     return res
 
 
